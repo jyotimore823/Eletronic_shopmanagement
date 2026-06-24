951a53fae870eb1815115a0e207b9fa9b5e009fe
Jyoti More <morejyoti823@gmail.com>
2026-06-24 09:32:49 -0400

    First Commit

---
+++ src/main/java/com/example/demo/EletronicShopmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletronicShopmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletronicShopmanagementApplication.class, args);
+	}
+
+}
